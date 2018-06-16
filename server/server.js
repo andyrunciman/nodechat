f810c7f80ef0b8ba6d4c20905d9def1a8223ca43
Andy Runciman <andyrunciman@gmail.com>
2018-06-16 12:36:44 +0100

    small changes to server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-
 const port = process.env.PORT || 3000;
-
-// console.log(__dirname + '/../public');
-console.log(publicPath);
-
 const app = express();
 
 app.use(express.static(publicPath));
